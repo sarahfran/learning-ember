790530abb0136018f2c66b92cb31120b32389eb6
Sarah Thompson <sarahfrances.thompson@gmail.com>
2016-05-05 20:01:37 +1200

    created templates for navbar and moved on to index controller

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('services');
+  this.route('news');
 });
 
 export default Router;
